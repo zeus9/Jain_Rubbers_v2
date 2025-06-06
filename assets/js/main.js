5a4fa9e17938771a6d60e1f482d6c8964f5f3b21
Sambhav Jain <sambhav1994jain@gmail.com>
2025-06-06 15:17:41 +0530

    Add font awesome angle up and down icons svg to nested dropdown in mobile nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,10 @@
     if(menu.length){
       menu.slicknav({
         prependTo: ".mobile_menu",
-        closedSymbol: '&#9661;',
-        openedSymbol: '&#9651;',
+        closedSymbol: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="9" height="9" fill="currentColor"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>',
+        openedSymbol: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="9" height="9" fill="currentColor"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>',
+        // closedSymbol: '+',
+        // openedSymbol: '-',
         label: '',
         removeIds: true,
         allowParentLinks: true,
